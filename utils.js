e53f43c501d73f92e1995b81fb47f2a7fac7a658
Ronnieraj37 <ronnierajsingh@gmail.com>
2025-04-16 14:36:09 +0530

    Add utility functions

---
+++ utils.js
@@ -0,0 +1,26 @@
+// Utility functions
+
+/**
+ * Add two numbers
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} The sum of a and b
+ */
+function add(a, b) {
+    return a + b;
+  }
+  
+  /**
+   * Subtract two numbers
+   * @param {number} a - First number
+   * @param {number} b - Second number
+   * @returns {number} The difference between a and b
+   */
+  function subtract(a, b) {
+    return a - b;
+  }
+  
+  module.exports = {
+    add,
+    subtract
+  };
